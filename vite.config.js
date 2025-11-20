0f3a3bbb8b6c7414658a9f0971bb5009d437195c
HadiaMushtaq-Dev <hadiamushtaq397@gmail.com>
2025-11-20 22:27:03 +0500

    fixed bug

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-   base: "/<Jobster>/",
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
+  base: "/Jobster/",
+  plugins: [react()],
 })
